f8fcf854ea48230dda3ac791b1f9b6083af44105
kaankaya <kaankaya.0707@gmail.com>
2022-12-14 17:46:11 +0300

    music app

---
+++ src/components/SongCard/SongCard.styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        padding:10,
+        flexDirection:'row',
+    },
+    image:{
+        width:100,
+        height:100,
+        borderRadius:50,
+    },
+    inner_container:{
+        flex:1,
+       padding:10,
+       justifyContent:'center',
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:27,
+    },
+    info_container:{
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        
+    },
+    year:{
+        marginLeft:10,
+        color:'gray',
+        fontWeight:'bold',
+        fontSize:12,
+    },
+    soldout_container:{
+        borderWidth:1,
+        borderColor:'red',
+        padding:5,
+        borderRadius:5,
+    },
+    content_container:{
+        flexDirection:'row',
+    },
+    soldout_title:{
+        color:'red',
+    },
+   
+});
